c1beb4a92729d269a6bf0864dc1a8c8ebd95590c
botbb4768 <botbb4768@gmail.com>
2024-10-08 20:09:28 +0500

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "https://api.github.com/users/token/repos"
+const BOT_NUMBER = "923239956476"
+const SESSION_ID = "PRABATH-MD~QJQHUJ5J#Wu2mjqVk9jIfSRlZmjvMsEURYA0ESheIw6cxVosw1ZE"
 
 
 module.exports = {
