81ef09d06c5e070f7eb582145a729ef81e79015a
sehanthilakarathna <www.sehanthilakarathna@gmail.com>
2024-11-08 16:55:24 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "+94769834458"
+const SESSION_ID = "PRABATH-MD~XLAhhZLA#cf3tOIzx8JE8llMy0HGaRfi7coo8U0Lze8yhOnNXJts"
 
 
 module.exports = {
