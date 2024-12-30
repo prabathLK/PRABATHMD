16b8eea9f999f4ad250d7a1b4c2a8efe1990ca6e
Vqjqkqkwkk <samithameedum92@gmail.com>
2024-12-30 16:59:05 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~jV1yWQZD#1gixJzXLoXC63S9a6aa_PiHkeZ2Bz9CS7Gb_tlme4d4"
 
 
 module.exports = {
